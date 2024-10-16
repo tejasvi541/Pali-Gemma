e1c9df231c57b0a4d7f94f5064c531f243e1276b
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-10-15 23:36:41 -0400

    Adding RMSNorm, on a break, thats why small committs

--- modelling_gemma.py
+++ modelling_gemma.py
@@ -71,3 +71,19 @@ class PaliGemmaConfig():
 
         self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size) ** 2
         self.vision_config.projection_dim = projection_dim
+
+class GemmaRMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.zeros(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float())
+        # Llama does x.to(float16) * w whilst Gemma is (x * w).to(float16)
+        # See https://github.com/huggingface/transformers/pull/29402
+        output = output * (1.0 + self.weight.float())
+        return output.type_as(x)
