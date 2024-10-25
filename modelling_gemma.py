8e19b5becdc47c6d9003abfc5be68c8e8651e2bf
Tejasvi <tejasvi.tanwar541@gmail.com>
2024-10-24 20:53:43 -0400

    Adding RMSNorm

--- modelling_gemma.py
+++ modelling_gemma.py
@@ -112,7 +112,22 @@ def rotate_half(x):
     x2 = x[..., x.shape[-1] // 2 :] # Takes the second half of the last dimension
     return torch.cat((-x2, x1), dim=-1)
 
+class GemmaRMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.zeros(dim))
 
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float())
+        # Llama does x.to(float16) * w whilst Gemma is (x * w).to(float16)
+        # See https://github.com/huggingface/transformers/pull/29402
+        output = output * (1.0 + self.weight.float())
+        return output.type_as(x)
+    
 def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
     cos = cos.unsqueeze(unsqueeze_dim) # Add the head dimension
     sin = sin.unsqueeze(unsqueeze_dim) # Add the head dimension
